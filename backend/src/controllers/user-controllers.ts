f91970ffd5541ae7840648c45687b4e42ce807b3
Apoorv <apoorv.r02@gmail.com>
2024-11-10 15:41:58 -0600

    [Feat] Add getAllUsers controller to retrieve all users

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,19 @@
-export const getAllUsers = () =>{
-    //get all users
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) =>{
+    try {
+        //get all users
+        const users = await User.find();
+        return res.status(200).json({message: "ok", users});
+
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({message: "ERROR", cause: error.message });
+    }
+
 };
