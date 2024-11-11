22322521080c64c59ac022949d16009f4dda7f9d
Apoorv <apoorv.r02@gmail.com>
2024-11-11 17:31:12 -0600

    [FIX] fix signup for email already in use

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -27,10 +27,34 @@ export const userSignup = async (
     try {
         //signing upo the users
         const { name, email, password} = req.body;
+        const existingUser = await User.findOne({email});
+        if (existingUser) 
+            return res.status(401).send("email already in use");
         const hashedPassword = await hash(password, 10);
         const user = new User({name, email, hashedPassword});
         await user.save();
         return res.status(200).json({message: "ok", id: user._id.toString() });
+
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({message: "ERROR", cause: error.message });
+    }
+
+};
+
+export const userLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) =>{
+    try {
+        //logging in the user
+        const { email, password} = req.body;
+        const user = await User.findOne({ email });
+        /*const hashedPassword = await hash(password, 10);
+        const user = new User({name, email, hashedPassword});
+        await user.save();
+        return res.status(200).json({message: "ok", id: user._id.toString() });*/
 
     } catch (error) {
         console.log(error)
